929ee06d5d8f1a6ae995bf05a6e9fbd8579c8118
Nahuel Defossé <D3f0@users.noreply.github.com>
2025-04-02 12:37:21 +0300

    Fix uv installation

--- src/pysetmeup/parts/uv.py
+++ src/pysetmeup/parts/uv.py
@@ -1,5 +1,12 @@
 from pyinfra.api import deploy
+from pyinfra import host, logger
+from pyinfra.operations import server
+from pyinfra.facts import server as server_facts
 
 
 @deploy("yq")
-def install(): ...
+def deploy():
+    if host.get_fact(server_facts.Which, "curl"):
+        server.shell("curl -LsSf https://astral.sh/uv/install.sh | sh")
+    else:
+        logger.warning("Skipping uv install because curl is not available")
