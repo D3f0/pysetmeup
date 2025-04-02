ce6adee05939b0c911e7bf7e4a36310e5c7a3d8c
Nahuel Defossé <D3f0@users.noreply.github.com>
2025-04-02 14:56:52 +0300

    Fix .bashrc.d creation and sourcing

--- src/pysetmeup/parts/bashrc_d_directory.py
+++ src/pysetmeup/parts/bashrc_d_directory.py
@@ -1,31 +1,46 @@
-import getpass
+"""
+Creates a ~/.bashrc.d/ directory and ensures that the ~/.basrc.d
+loads the scripts that are placed in there.
+"""
 
 from pyinfra import host
 from pyinfra.api import deploy
 from pyinfra.operations import files
+from pyinfra.facts.server import Home, Command
 from textwrap import dedent
 
 
 @deploy(name="Create ~/.bashrc.d directory")
 def deploy():
-    user = host.data.get("user", getpass.getuser())
+    user = host.data.get("user", host.get_fact(Command, "whoami"))
+    home = host.get_fact(Home, user=user)
+    mounted_mac = host.get_fact(Command, "test -d /Users/ && echo OK || echo NO")
+
+    # In lima vms the su command can't be used
+    kw_args = {}
+    if mounted_mac and home.endswith(".linux"):
+        kw_args["sudo"] = True
 
     files.directory(
         name="Create ~/.bashrc.d/ for organizing bash customizations",
-        path=f"/home/{user}/.bashrc.d/",
+        path=f"{home}/.bashrc.d/",
         user=user,
-        _su_user=user,
+        # _su_user=user,
     )
 
     files.block(
         name="Ensure ~/.bashrc sources all the files in ~/.bashrc.d/*",
-        path=f"/home/{user}/.bashrc",
+        path=f"{home}/.bashrc",
         content=dedent(
             """
             if [ -d ~/.bashrc.d ]; then
-                    for file in ~/.bashrc.d/*; do
-                            source $file
-                    done
+                shopt -s nullglob  # Make globs expand to nothing if no matches
+                for file in ~/.bashrc.d/*; do
+                    if [ -f "$file" ]; then  # Check if it's a regular file
+                        source "$file"
+                    fi
+                done
+                shopt -u nullglob  # Turn off nullglob when done
             fi
             """
         ),
