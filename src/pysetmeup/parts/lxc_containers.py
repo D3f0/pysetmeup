151eb960b7f3893c924ec258a7500111bd0db417
Nahuel Defossé <D3f0@users.noreply.github.com>
2025-04-02 15:51:45 +0300

    Create LXC containers

---
+++ src/pysetmeup/parts/lxc_containers.py
@@ -0,0 +1,60 @@
+from textwrap import dedent
+from pyinfra.api import deploy
+from pyinfra import host, logger
+from . import lxc
+from pyinfra.operations import server
+from pyinfra.facts.server import Command
+
+
+@deploy(name="Setup LXC containers with LVM backingstore")
+def deploy():
+    """
+    Creates an LXC container based by LVM partition.
+
+    An example of the host data:
+    ```
+    app_servers = [("host", {"lxc_containers": [{"name": "ubuntu"}]})]
+    ```
+
+
+    """
+    containers: list[dict] = host.data.get("lxc_containers")
+    if containers:
+        lxc.deploy()
+    for container in containers:
+        name = container.get("name")
+        if not name:
+            logger.warning(
+                f"Skipping container config {container} because of missing name"
+            )
+            continue
+        create = (
+            host.get_fact(Command, f"lxc-ls -1 | grep {name} || echo missing")
+            == "missing"
+        )
+        if create:
+            distro = container.get("distro", "ubuntu")
+            release = container.get("release", "focal")
+            arch = container.get("arch", "amd64")
+            volume_group = container.get("volume_group")
+            size = container.get("size", "50G")
+
+            if not volume_group:
+                volume_group = host.get_fact(
+                    Command, "vgs --noheadings -o vg_name"
+                ).strip()
+
+            command = dedent(f"""
+                lxc-create -n {name} -t download -B lvm
+                    --lvname rootfs-{name} \
+                    --vgname {volume_group} \
+                    --fssize {size} \
+                    -- \
+                    --dist {distro} \
+                    --release {release} \
+                    --arch {arch}
+
+            """).replace("\n", " ")
+            server.shell(command, name=f"Creating LXC container {name}")
+        # Start the container if already created
+        server.shell(f"lxc-start {name}", name=f"Staring container {name}")
