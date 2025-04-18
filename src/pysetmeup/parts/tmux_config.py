61763ae38857002aac4ce8496defcb21422d5c7f
Nahuel Defossé <D3f0@users.noreply.github.com>
2025-04-18 09:20:25 +0300

    tmux config separated from user deployment

---
+++ src/pysetmeup/parts/tmux_config.py
@@ -0,0 +1,43 @@
+from textwrap import dedent
+from pyinfra import host
+from pyinfra.api import deploy
+from pyinfra.operations import git, server
+from pyinfra.facts.server import Home
+
+
+@deploy("Set tmux config")
+def deploy(user: str | None = ""):
+    """
+    Sets the tmux configuration form https://github.com/gpakosz/.tmux
+    """
+    user = user or host.data.get(
+        "user",
+    )
+    home = host.get_fact(Home, user=user)
+    dest = f"{home}/.tmux"
+    git.repo(
+        name="tmux configuration from gpakosz",
+        src="https://github.com/gpakosz/.tmux.git",
+        dest=dest,
+        branch="master",
+        user=user,
+        _su_user=user,
+    )
+    server.shell(
+        dedent(f"""
+        if [ ! -f {home}/.tmux.conf ]; then
+            ln -sf {dest}/.tmux.conf  {home}/
+        fi
+        """),
+        name=f"Setting .tmux.conf in {home} if not present...",
+        _su_user=user,
+    )
+    server.shell(
+        dedent(f"""
+        if [ ! -f {home}/.tmux.conf.local ]; then
+            cp {dest}/.tmux.conf.local  {home}/
+        fi
+        """),
+        name=f"Setting .tmux.conf.local in {home} if not present...",
+        _su_user=user,
+    )
