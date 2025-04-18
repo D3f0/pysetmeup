ecadbd73b9fbb2e653175bda6bd3eaf085fdc4cb
Nahuel Defossé <D3f0@users.noreply.github.com>
2025-04-18 09:19:19 +0300

    uv fixes

--- src/pysetmeup/parts/uv.py
+++ src/pysetmeup/parts/uv.py
@@ -4,9 +4,12 @@ from pyinfra.operations import server
 from pyinfra.facts import server as server_facts
 
 
-@deploy("yq")
-def deploy():
+@deploy("uv")
+def deploy(user: str | None = ""):
+    user = user or host.data.get(
+        "user",
+    )
     if host.get_fact(server_facts.Which, "curl"):
-        server.shell("curl -LsSf https://astral.sh/uv/install.sh | sh")
+        server.shell("curl -LsSf https://astral.sh/uv/install.sh | sh", _su_user=user)
     else:
         logger.warning("Skipping uv install because curl is not available")
