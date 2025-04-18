fe2c2489e10d9724a44e3b0e39a0bb3c8e087cee
Nahuel Defossé <D3f0@users.noreply.github.com>
2025-04-18 09:23:35 +0300

    gh cli updates

--- src/pysetmeup/parts/gh.py
+++ src/pysetmeup/parts/gh.py
@@ -5,10 +5,15 @@ from pyinfra.operations import server
 
 
 @deploy(name="Install github CLI")
-def deploy():
+def deploy(user: str = None):
     gh_location = host.get_fact(Which, "gh")
+    user = user or host.data.get("user")
     if not gh_location:
-        server.shell("curl -sS https://webi.sh/gh | sh")
+        server.shell(
+            "curl -sS https://webi.sh/gh | sh",
+            _su_user=user,
+            name="Installing github CLI",
+        )
 
 
 if __name__ in {"builtins", "__main__"}:
